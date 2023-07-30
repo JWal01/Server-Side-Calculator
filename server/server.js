b022bf69885d99b51967d56bdfb0800630d3cfbc
Jeff Walter <Jeff.Walter@me.com>
2023-07-30 10:14:17 -0500

    Set up server and it is running

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("Script Running");
+
+let express = require('express');
+let app = express();
+const port = 5001;
+
+app.use(express.json());
+
+// const quoteList = require('./quoteList');
+
+app.use(express.static('server/public'));
+
+
+app.listen(port, function() {
+  console.log('listening on port', port);
+})
